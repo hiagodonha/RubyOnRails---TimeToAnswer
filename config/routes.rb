42b233bf0d42163625ceca413b16c8e95fdec39f
hiagodonha <hiagodefenix@gmail.com>
2023-02-24 18:58:56 -0300

    melhorias em listar, update e salvar adm

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   namespace :admins_backoffice do
-    get 'admins/index'
+    get 'welcome/index'
+    resources :admins, only: [:index, :edit, :update] 
   end
   namespace :site do
     get 'welcome/index'
   end
   namespace :users_backoffice do
-    get 'welcome/index'
-  end
-  namespace :admins_backoffice do
     get 'welcome/index'
   end
   devise_for :admins
